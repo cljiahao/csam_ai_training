772f4e6250ecd6b3cbba59c4fdc928be575ea3df
CLJiaHao <clarence.lee@murata.com>
2023-12-20 17:12:59 +0800

    Added Menu for navigating to other pages

--- frontend/src/pages/CDA/CDA.js
+++ frontend/src/pages/CDA/CDA.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 import { Transition } from "@headlessui/react";
 import { AppContext } from "../../contexts/context";
 
+import { initialSettings } from "../../core/config";
 import Gallery from "../../containers/Gallery/Gallery";
 import ButtonCont from "../../containers/ButtonCont/ButtonCont";
 import getTrackbar from "../../utils/getTrackbar";
 import Trackbars from "../../containers/Trackbars/Trackbars";
+import Menu from "../../containers/Menu/Menu";
 
 function CDA() {
   const [random, setRandom] = useState([]);
   const [range, setRange] = useState({});
   const [inText, setInText] = useState({});
+  const [settings, setSettings] = useState(initialSettings);
 
   useEffect(() => {
     const getTrack = async () => {
@@ -21,6 +24,10 @@ function CDA() {
     getTrack();
   }, []);
 
+  const openMenu = () => {
+    setSettings({ ...settings, menu: !settings.menu });
+  };
+
   return (
     <AppContext.Provider
       value={{ random, setRandom, range, setRange, inText, setInText }}
@@ -29,9 +36,20 @@ function CDA() {
         <section className="h-full w-[67%]">
           <Gallery />
         </section>
-        <aside className="h-full w-[33%]">
-          <ButtonCont />
+        <aside className="relative flex h-full w-[33%] flex-col">
+          <ButtonCont openMenu={openMenu} />
           <Trackbars />
+          <Transition
+            show={settings.menu}
+            enter="transition-opacity ease-in duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-in duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Menu openMenu={openMenu} />
+          </Transition>
         </aside>
       </div>
     </AppContext.Provider>
