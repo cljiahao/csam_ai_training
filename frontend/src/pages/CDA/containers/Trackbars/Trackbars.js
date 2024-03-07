0426e779ed8038b2bc90c05b40a5d6319b69b1bb
CLJiaHao <clarence.lee@murata.com>
2024-03-07 10:05:00 +0800

    Fixed trackbar size to fit entry boxes

--- frontend/src/pages/CDA/containers/Trackbars/Trackbars.js
+++ frontend/src/pages/CDA/containers/Trackbars/Trackbars.js
@@ -6,19 +6,21 @@ const Trackbars = () => {
   const { range } = useContext(AppContext);
 
   return (
-    <div className="w-full 2xl:text-xl">
+    <div className="w-full space-y-1 text-sm 2xl:text-xl">
       {Object.keys(range.slider).map((type) => {
         return (
           <div
-            className="flex-col rounded-xl bg-gray-400 p-3 2xl:p-5"
+            className="flex flex-col rounded-xl bg-gray-400 p-3 2xl:p-5"
             key={type}
           >
             <div className="font-bold">
               {`${type ? type[0].toUpperCase() + type.slice(1) : ""} Trackbars`}
             </div>
-            {Object.keys(range.slider[type]).map((label) => {
-              return <Slider type={type} label={label} key={label} />;
-            })}
+            <div className="flex flex-col space-y-1">
+              {Object.keys(range.slider[type]).map((label) => {
+                return <Slider type={type} label={label} key={label} />;
+              })}
+            </div>
           </div>
         );
       })}
