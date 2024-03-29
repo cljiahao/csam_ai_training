9ad239b5e4990ae1880157a4966edde411e45ee7
Perry Ong Wen Qing <MES21159@murata.com>
2024-03-29 10:26:26 +0800

    swap predicted and value column

--- frontend/src/pages/CMT/containers/Evaluation/components/List/components/Card/Card.js
+++ frontend/src/pages/CMT/containers/Evaluation/components/List/components/Card/Card.js
@@ -4,8 +4,8 @@ const Card = ({ name, value, pred }) => {
   return (
     <div className="flex w-full rounded-lg border border-slate-400 py-1 pl-3 pr-6 text-center 2xl:py-2">
       <div className="mr-auto text-right">{name}</div>
-      <div className="w-20 2xl:w-28">{value}</div>
       <div className="w-20 2xl:w-28">{pred}</div>
+      <div className="w-20 2xl:w-28">{value}</div>
     </div>
   );
 };
