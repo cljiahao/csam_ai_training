df08dfbd5a3ec122a1f34682c382aa5555d8e2ba
CLJiaHao <clarence.lee@murata.com>
2024-04-19 17:48:46 +0800

    moved api calls to have better readability

--- frontend/src/utils/api_misc.js
+++ frontend/src/utils/api_misc.js
@@ -10,14 +10,3 @@ export const getItemType = async () => {
   const json = await resp.json();
   return json;
 };
-
-export const zipModel = async (model_path) => {
-  const resp = await fetch(`${API}/CMT/zip_model`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ m_path: model_path }),
-  });
-  return resp;
-};
