010b8911023b5bb69a3684e7c9faf0e41a03903d
CLJiaHao <clarence.lee@murata.com>
2025-03-03 17:14:46 +0800

    FEAT: base utils copied from another repo to re-use

--- backend/src/utils/debug.py
+++ backend/src/utils/debug.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from datetime import timedelta
 
+from core.exceptions import CustomErrorMessage
 from core.logging import logger
 
 
@@ -22,18 +23,56 @@ def cvWin(image: np.ndarray, name: str = "image") -> None:
         cv2.destroyAllWindows()
 
 
-def timer(print_message=""):
+def timer(print_message: str = ""):
+    """A decorator to log the time taken by a function.
+
+    Args:
+        print_message : str
+            A custom message to be logged along with the elapsed time.
+    """
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             start_time = time.time()
             result = func(*args, **kwargs)
             end_time = time.time()
             elapsed_time = timedelta(seconds=end_time - start_time)
+            formatted_time = str(elapsed_time).split(".")[0]
             if print_message:
-                logger.info(f"{print_message} took: {elapsed_time}", stacklevel=2)
+                logger.info(f"{print_message} took: {formatted_time}", stacklevel=2)
             else:
-                logger.info(f"Total time taken: {elapsed_time}", stacklevel=2)
+                logger.info(f"Total time taken: {formatted_time}", stacklevel=2)
             return result
+
+        return wrapper
+
+    return decorator
+
+
+def error_handler(
+    print_message: str = "",
+    custom_error: Exception = None,
+):
+    """A decorator to log exceptions that occur during the execution of a function.
+
+    Args:
+        print_message (str, optional): A custom message to be logged along with the exception details.
+        custom_error (Exception, optional): A custom exception class to raise instead of the default exception.
+
+    """
+
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except CustomErrorMessage as e:
+                if custom_error:
+                    raise custom_error(str(e))
+                raise Exception(str(e))
+            except Exception as e:
+                if custom_error:
+                    raise custom_error(print_message if print_message else str(e))
+                raise e
 
         return wrapper
 
