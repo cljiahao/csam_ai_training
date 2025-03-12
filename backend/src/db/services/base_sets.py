86beb882e0fa79a30bea6f3b4018ee8620e461df
CLJiaHao <clarence.lee@murata.com>
2025-03-12 15:13:16 +0800

    FEAT: add base and eval sets db services to store count data

--- backend/src/db/services/base_sets.py
+++ backend/src/db/services/base_sets.py
@@ -9,30 +9,43 @@ from db.repository.base_sets import BaseSetsRepository
 class BaseSetsService:
     def __init__(self, db: Session):
         """Initialize service with repository."""
-        self.base_sets_repo = BaseSetsRepository(db)
+        self.repo = BaseSetsRepository(db)
 
-    def _read_base_sets(self, item: str) -> BaseSets:
+    def _validate_base_sets_keys(self, base_sets_data: dict) -> None:
+        """Validate the keys in the image settings data."""
+        valid_keys = set([c.value for c in ClassLabel])
+
+        invalid_keys = set(base_sets_data) - valid_keys
+        if invalid_keys:
+            raise InvalidInputError(
+                f"Unknown keys in image settings data: {', '.join(invalid_keys)}"
+            )
+
+    def read_base_sets(self, item: str) -> BaseSets:
         """Service layer method to read base sets"""
+        if not item:
+            raise InvalidInputError("Filter conditions: Item cannot be empty.")
         filter_condition = {"item": item}
 
-        return self.base_sets_repo.read_base_sets(filter_condition)
+        return self.repo.read_base_sets(filter_condition)
 
     def create_or_update_base_sets(
         self, item: str, base_sets_data: dict[str, int]
     ) -> BaseSets:
         """Service layer method to create new or update base sets"""
         if not item:
-            raise InvalidInputError()
-        if set(base_sets_data) - set([c.value for c in ClassLabel]):
-            raise InvalidInputError()
+            raise InvalidInputError("Filter conditions: Item cannot be empty.")
+
+        self._validate_base_sets_keys(base_sets_data)
 
         data_condition = {"item": item}
         new_base_sets_data = {
             f"no_of_{k.lower()}": v for k, v in base_sets_data.items()
         }
 
-        if not self._read_base_sets(item):
+        existing_base_sets = self.read_base_sets(item)
+        if not existing_base_sets:
             new_base_sets_data.update(data_condition)
-            return self.base_sets_repo.create_base_sets(new_base_sets_data)
+            return self.repo.create_base_sets(new_base_sets_data)
 
-        return self.base_sets_repo.update_base_sets(data_condition, new_base_sets_data)
+        return self.repo.update_base_sets(data_condition, new_base_sets_data)
