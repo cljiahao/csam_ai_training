f6bd931e7120fe94d3155584e379cadfe6c86020
CLJiaHao <clarence.lee@murata.com>
2025-03-17 15:48:18 +0800

    FEAT: create api route for retrieving summary data

---
+++ backend/src/apis/v2/schemas/summary.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BaseSetsData:
+    no_of_g: int = 0
+    no_of_ng: int = 0
+    no_of_others: int = 0
+
+
+@dataclass
+class EvalBaseKey:
+    total_sum: int = 0
+
+
+@dataclass
+class EvalSetsData:
+    colors_count: EvalBaseKey
+    thousands_count: EvalBaseKey
+    mass_pro_count: EvalBaseKey
+
+
+@dataclass
+class EvalBaseSets:
+    item: str
+    eval_data: EvalSetsData
+    base_data: BaseSetsData
+
+
+@dataclass
+class EvalBaseSetsData:
+    augment_multiplier: int
+    mass_pro_threshold: int
+    colors_threshold: int
+    thousands_threshold: int
+    eval_base_sets: list[EvalBaseSets]
