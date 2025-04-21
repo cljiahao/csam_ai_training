7f7917a57283d9ef7d774f855d16320557471ec1
CLJiaHao <clarence.lee@murata.com>
2025-04-21 14:17:22 +0800

    FEAT: add unique arg to item under base sets model

--- backend/src/db/models/base_sets.py
+++ backend/src/db/models/base_sets.py
@@ -10,7 +10,7 @@ class BaseSets(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4, index=True)
     date_created: Mapped[dt] = mapped_column(default=func.now())
     date_updated: Mapped[dt] = mapped_column(default=func.now(), onupdate=func.now())
-    item: Mapped[str] = mapped_column(index=True)
+    item: Mapped[str] = mapped_column(unique=True, index=True)
     no_of_g: Mapped[int] = mapped_column(default=0)
     no_of_ng: Mapped[int] = mapped_column(default=0)
     no_of_others: Mapped[int] = mapped_column(default=0)
