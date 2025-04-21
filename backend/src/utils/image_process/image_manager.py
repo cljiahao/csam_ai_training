2b7f3b906dcb49532c69a034f5edcbcf778e8922
CLJiaHao <clarence.lee@murata.com>
2025-04-21 15:26:46 +0800

    DOCS: improve docstrings

--- backend/src/utils/image_process/image_manager.py
+++ backend/src/utils/image_process/image_manager.py
@@ -9,13 +9,19 @@ from utils.debug import error_handler
 
 
 class ImageManager:
+    """A utility class for managing image-related operations."""
+
     @staticmethod
     def archive_existing_file(folder_path: Path, file_name: str) -> None:
-        """Archives an existing file in the specified folder by renaming it with an index."""
+        """Archives an existing file in the specified folder by renaming it with an index.
+
+        Args:
+            folder_path: The path to the folder containing the file.
+            file_name: The name of the file to archive.
+        """
         files_in_dir = list(folder_path.glob("*"))
         file_path = folder_path / file_name
         if files_in_dir and file_path.exists():
-            # Rename file with same filename to archive it
             archive_name = file_path.stem + f"_{len(files_in_dir)}" + file_path.suffix
             archived_file_path = folder_path / archive_name
             file_path.rename(archived_file_path)
@@ -27,7 +33,14 @@ class ImageManager:
         custom_error=ImageProcessError,
     )
     def file_to_image(file: UploadFile) -> np.ndarray:
-        """Converts an uploaded file to an OpenCV image."""
+        """Converts an uploaded file to an OpenCV image.
+
+        Args:
+            file: The uploaded file.
+
+        Returns:
+            The OpenCV image as a NumPy array.
+        """
         file_content = file.file.read()
         np_image = np.frombuffer(file_content, dtype=np.uint8)
         image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
@@ -41,7 +54,14 @@ class ImageManager:
         custom_error=ImageProcessError,
     )
     def path_to_image(path: Path) -> np.ndarray:
-        """Read from path into an OpenCV image."""
+        """Reads an image from a file path into an OpenCV image.
+
+        Args:
+            path: The path to the image file.
+
+        Returns:
+            The OpenCV image as a NumPy array.
+        """
         image = cv2.imread(str(path))
         if image is None:
             raise Exception()
@@ -53,7 +73,12 @@ class ImageManager:
         custom_error=ImageProcessError,
     )
     def save_image(file_path: str | Path, image: np.ndarray) -> None:
-        """Saves a NumPy array as an image file."""
+        """Saves a NumPy array as an image file.
+
+        Args:
+            file_path: The path to save the image file.
+            image: The image as a NumPy array.
+        """
         if isinstance(file_path, Path):
             file_path = str(file_path)
         cv2.imwrite(file_path, image)
