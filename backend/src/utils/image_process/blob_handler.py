89b3c1ffbeb9c54aaac985ad0305c886c4b4d080
CLJiaHao <clarence.lee@murata.com>
2025-04-21 15:12:48 +0800

    DOCS: add more docstrings and improve function names to better understand its use

--- backend/src/utils/image_process/blob_handler.py
+++ backend/src/utils/image_process/blob_handler.py
@@ -3,14 +3,35 @@ import numpy as np
 
 
 class BlobHandler:
-    """A utility class for blob found from contours of images."""
+    """A utility class for processing blobs (regions of interest) found from contours in images."""
+
+    @staticmethod
+    def create_kernel(kernel_size: int) -> np.ndarray:
+        """Creates a square kernel of ones for morphological operations.
+
+        Args:
+            kernel_size: The size (width and height) of the square kernel.
+
+        Returns:
+            A NumPy ndarray representing the square kernel with uint8 data type.
+        """
+        return np.ones((kernel_size, kernel_size), dtype=np.uint8)
 
     @staticmethod
     def crop_roi(
         image: np.ndarray, x_center: float, y_center: float, padding: int
     ) -> np.ndarray:
-        """Crops a region of interest (ROI) from the given image based on a rotated rectangle."""
-        # Ensure the crop does not go out of image bounds
+        """Crops a region of interest (ROI) from the given image.
+
+        Args:
+            image: The input image.
+            x_center: The x-coordinate of the center of the ROI.
+            y_center: The y-coordinate of the center of the ROI.
+            padding: The padding around the center for the ROI.
+
+        Returns:
+            The cropped ROI.
+        """
         y_min = int(max(0, y_center - padding))
         y_max = int(min(image.shape[0], y_center + padding))
         x_min = int(max(0, x_center - padding))
@@ -19,10 +40,18 @@ class BlobHandler:
         return image[y_min:y_max, x_min:x_max]
 
     @staticmethod
-    def erode_and_find_contours(
-        image: np.ndarray, max_kernel_size: int = 15
+    def split_blobs_with_erosion(
+        image: np.ndarray, max_kernel_size: int = 50
     ) -> list[np.ndarray]:
-        """Applies erosion on the image with varying kernel sizes to attempt splitting contours."""
+        """Applies erosion and finds contours to attempt splitting blobs.
+
+        Args:
+            image: The input image.
+            max_kernel_size: The maximum kernel size for erosion.
+
+        Returns:
+            A list of contours if found, otherwise an empty list.
+        """
         copy_image = image.copy()
         for x_coords in range(1, max_kernel_size + 1):
             for y_coords in range(1, max_kernel_size + 1):
