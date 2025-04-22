f9efb3a687558021bf761d522cab610702e89d7e
CLJiaHao <clarence.lee@murata.com>
2025-04-22 17:48:03 +0800

    FEAT: separate finder loop to be batch and chip exclusive, improving algorithm to find

--- backend/src/apis/v2/logic/auto_settings_finder.py
+++ backend/src/apis/v2/logic/auto_settings_finder.py
@@ -1,3 +1,4 @@
+import cv2
 import math
 from fastapi import UploadFile
 from sqlalchemy.orm import Session
@@ -6,7 +7,8 @@ from apis.v2.components.extract_image_coords import (
     extract_batch_coords,
     extract_chip_coords,
 )
-from apis.v2.components.finder_loop import finder_loop
+from apis.v2.components.finder_loop import batch_finder, chip_finder
+from apis.v2.helpers.image_process_utils import create_border
 from apis.v2.schemas.settings import FileDataLists
 from db.services.image_settings import ImageSettingsService
 from utils.debug import timer
@@ -21,40 +23,32 @@ def auto_settings_finder(
 
     image = ImageManager.file_to_image(file)
 
-    erode_value, close_value, contour_info_list = finder_loop(
-        image, target_count, is_batch
-    )
+    _, border_gray, border_blank, _ = create_border(image)
+    _, binary_image = cv2.threshold(border_gray, 250, 255, cv2.THRESH_BINARY_INV)
 
-    write_to_db(
-        db,
-        item,
-        erode_value,
-        close_value,
-        contour_info_list.get_average_length(),
-        is_batch,
-    )
+    if is_batch:
+        erode_value, close_value, contour_info_list = batch_finder(
+            binary_image, target_count
+        )
+
+        settings_data = {"batch_erode": erode_value, "batch_close": close_value}
+    else:
+        noise_erode_value, dilate_value, erode_value, contour_info_list = chip_finder(
+            binary_image, border_blank, target_count
+        )
+        settings_data = {
+            "chip_noise_erode": noise_erode_value,
+            "chip_dilate": dilate_value,
+            "chip_erode": erode_value,
+            "crop_size": math.ceil(contour_info_list.get_average_length() * 2),
+        }
+
+    image_settings_service = ImageSettingsService(db)
+    image_settings_service.create_or_update_image_settings(item, settings_data)
 
     data_list = extract_coords(image.shape[:2], contour_info_list, is_batch)
 
     return FileDataLists(data_files=data_list)
-
-
-@timer("Writing to Database")
-def write_to_db(
-    db: Session, item: str, erode: int, close: int, average_length: int, is_batch: bool
-) -> None:
-    """Writes lot and chip details to the database."""
-
-    settings_data = {
-        "batch_erode" if is_batch else "chip_erode": erode,
-        "batch_close" if is_batch else "chip_close": close,
-    }
-
-    if not is_batch and average_length:
-        settings_data["crop_size"] = math.ceil(average_length * 2)
-
-    image_settings_service = ImageSettingsService(db)
-    image_settings_service.create_or_update_image_settings(item, settings_data)
 
 
 @timer("Extracting Coordinates")
