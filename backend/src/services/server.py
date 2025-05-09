b8b210d6bb5e8c063ab6226e0a736d58417337aa
CLJiaHao <clarence.lee@murata.com>
2025-05-09 15:31:55 +0800

    RFCT: re-arrange lines for better readability and resource usage

--- backend/src/services/server.py
+++ backend/src/services/server.py
@@ -14,7 +14,6 @@ API_INSTALL_MODEL_ENDPOINT = "/api/v2/upload/install_model"
 @error_handler()
 def post_model_files(item: str, ai_model_name: str) -> any:
     """Posts model files (label and model) to the AI server for installation."""
-    api_client = APIClient(service_settings.AI_SERVER_URL)
     file_name = Path(ai_model_name).stem
     label_file_path = dm.model_dir / item / f"{file_name}.txt"
     model_file_path = dm.model_dir / item / ai_model_name
@@ -30,6 +29,9 @@ def post_model_files(item: str, ai_model_name: str) -> any:
     }
 
     url = f"{API_INSTALL_MODEL_ENDPOINT}?item={item}"
+    api_client = APIClient(service_settings.AI_SERVER_URL)
     result = api_client.post_files(url, file_path_list=file_path_list)
+
     logger.info(f"Successfully posted model files ({ai_model_name}) to {url}")
+
     return result
