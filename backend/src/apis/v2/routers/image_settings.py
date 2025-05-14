4d4a48e9163317decb908460d2f1526f66fc0268
CLJiaHao <clarence.lee@murata.com>
2025-05-14 17:23:44 +0800

    BUG: fix class not subscribable with image_settings

--- backend/src/apis/v2/routers/image_settings.py
+++ backend/src/apis/v2/routers/image_settings.py
@@ -28,12 +28,12 @@ def get_image_settings(
     image_settings_service = ImageSettingsService(db)
     image_settings = image_settings_service.read_image_settings(item)
     return {
-        "batch_erode": image_settings["batch_erode"],
-        "batch_close": image_settings["batch_close"],
-        "chip_noise_erode": image_settings["chip_noise_erode"],
-        "chip_dilate": image_settings["chip_dilate"],
-        "chip_erode": image_settings["chip_erode"],
-        "crop_size": image_settings["crop_size"],
+        "batch_erode": image_settings.batch_erode,
+        "batch_close": image_settings.batch_close,
+        "chip_noise_erode": image_settings.chip_noise_erode,
+        "chip_dilate": image_settings.chip_dilate,
+        "chip_erode": image_settings.chip_erode,
+        "crop_size": image_settings.crop_size,
     }
 
 
