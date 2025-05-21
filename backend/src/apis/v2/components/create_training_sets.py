00c6e6a8766f4f27bdae32236c2dfefd6dae0867
CLJiaHao <clarence.lee@murata.com>
2025-05-21 15:25:11 +0800

    FEAT: edit base set creation into training set to expand into retrain sets

--- backend/src/apis/v2/components/create_training_sets.py
+++ backend/src/apis/v2/components/create_training_sets.py
@@ -8,10 +8,11 @@ from apis.v2.constants.csam_thresholds import AugmentThresholdRatio
 from apis.v2.constants.datasets_thresholds import DatasetSummaryThresholds
 from apis.v2.schemas.csam_image import LabeledImageData
 from constants.colors import CSAMcolor
-from constants.folder_names import BaseSetsFolderName
+from constants.folder_names import BaseSetsFolderName, ReTrainFolderName
 from core.directory_manager import directory_manager as dm
 from core.logging import logger
 from db.services.base_sets import BaseSetsService
+from db.services.retrain_sets import ReTrainSetsService
 from utils.debug import timer
 from utils.image_process.image_manager import ImageManager
 
@@ -21,8 +22,9 @@ def populate_base_folders(
     item: str, image_data_list: list[LabeledImageData], db: Session
 ) -> None:
     """Populates base folders with images based on labels."""
+
     label_image_dict = group_image_data_by_label(image_data_list)
-    base_dir = setup_base_sets_environment(item, label_image_dict)
+    base_dir = setup_folder_environment(item, label_image_dict)
     label_folder_counts = dm.count_files_in_subdirectories(
         base_dir, label_image_dict.keys()
     )
@@ -32,6 +34,43 @@ def populate_base_folders(
         if label == BaseSetsFolderName.NG:
             augment_multiplier = len(CSAMcolor) * AugmentThresholdRatio.BASE_MULTIPLIER
             max_file_count = math.floor(max_file_count / augment_multiplier)
+
+        required_count = max_file_count - label_folder_counts[label]
+
+        if required_count == 0:
+            logger.info(
+                f"Label: {label} already has the required {max_file_count} files."
+            )
+            continue
+
+        if required_count < len(image_data_list):
+            random.shuffle(image_data_list)
+
+        images_to_save = image_data_list[:required_count]
+        for image_data in images_to_save:
+            ImageManager.save_image(
+                base_dir / label / image_data.file_name, image_data.image_data
+            )
+        label_folder_counts[label] += len(images_to_save)
+        logger.info(f"label: {label} has {label_folder_counts[label]} files.")
+
+    base_sets_service = BaseSetsService(db)
+    base_sets_service.create_or_update_base_sets(item, label_folder_counts)
+
+
+@timer("Populating Re-Train Folders")
+def populate_retrain_folders(
+    item: str, image_data_list: list[LabeledImageData], db: Session
+) -> None:
+    """Populates re-train folders with images based on labels."""
+    label_image_dict = group_image_data_by_label(image_data_list)
+    base_dir = setup_folder_environment(item, label_image_dict)
+    label_folder_counts = dm.count_files_in_subdirectories(
+        base_dir, label_image_dict.keys()
+    )
+
+    for label, image_data_list in label_image_dict.items():
+        max_file_count = DatasetSummaryThresholds.MAX_DATASET
 
         required_count = max_file_count - label_folder_counts[label]
         if required_count == 0:
@@ -50,8 +89,17 @@ def populate_base_folders(
         label_folder_counts[label] += len(images_to_save)
         logger.info(f"label: {label} has {label_folder_counts[label]} files.")
 
-    base_sets_service = BaseSetsService(db)
-    base_sets_service.create_or_update_base_sets(item, label_folder_counts)
+    retrain_sets_service = ReTrainSetsService(db)
+    retrain_sets_service.create_or_update_retrain_sets(item, label_folder_counts)
+
+
+def setup_folder_environment(
+    base_folder: str, item: str, label_image_dict: dict[str, list[LabeledImageData]]
+) -> Path:
+    """Sets up the re-train sets directory environment."""
+    retrain_dir = dm.images_dir / base_folder / item
+    dm.create_subdirectories(retrain_dir, label_image_dict.keys())
+    return retrain_dir
 
 
 def group_image_data_by_label(
@@ -62,12 +110,3 @@ def group_image_data_by_label(
     for image_data in image_data_list:
         label_image_dict[image_data.label_mode].append(image_data)
     return label_image_dict
-
-
-def setup_base_sets_environment(
-    item: str, label_image_dict: dict[str, list[LabeledImageData]]
-) -> Path:
-    """Sets up the base sets directory environment."""
-    base_dir = dm.images_dir / BaseSetsFolderName.BASE / item
-    dm.create_subdirectories(base_dir, label_image_dict.keys())
-    return base_dir
