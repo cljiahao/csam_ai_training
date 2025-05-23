86e1fb3056e8d3a676061d6122f9bd24871879bc
CLJiaHao <clarence.lee@murata.com>
2025-05-23 17:28:35 +0800

    BUG: fix the column name to fit no_of_good instead of no_of_g

--- backend/src/db/models/base_sets.py
+++ backend/src/db/models/base_sets.py
@@ -10,7 +10,7 @@ class BaseSets(Base):
     date_created: Mapped[dt] = mapped_column(default=func.now())
     date_updated: Mapped[dt] = mapped_column(default=func.now(), onupdate=func.now())
     item: Mapped[str] = mapped_column(unique=True, index=True)
-    no_of_g: Mapped[int] = mapped_column(default=0)
+    no_of_good: Mapped[int] = mapped_column(default=0)
     no_of_ng: Mapped[int] = mapped_column(default=0)
     no_of_others: Mapped[int] = mapped_column(default=0)
 
