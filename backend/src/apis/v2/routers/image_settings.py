c42447cfaa5bae88406da9e686759d21ae2592ca
CLJiaHao <clarence.lee@murata.com>
2025-05-26 09:36:57 +0800

    RFCT: correct routing url with query.

--- backend/src/apis/v2/routers/image_settings.py
+++ backend/src/apis/v2/routers/image_settings.py
@@ -14,7 +14,7 @@ router = APIRouter()
 
 
 @router.get(
-    "/",
+    "",
     response_model=dict[str, int],
     summary="Return image settings found in database",
     operation_id="ImageSettings",
