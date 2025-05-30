6e30e57358a80c1d1a32015943692aa5c150447c
CLJiaHao <clarence.lee@murata.com>
2025-05-30 11:52:46 +0800

    FEAT: add more query and mutation keys

--- frontend/src/constants/api-keys.js
+++ frontend/src/constants/api-keys.js
@@ -1,9 +1,15 @@
 export const QUERY_KEYS = {
-  API_SETTINGS: "settingsFound",
+  API_AUGMENT: "augmentData",
+  API_DATATABLE: "dataTable",
   API_EVALUATE: "evaluateModel",
+  API_SETTINGS: "settingsFound",
+  API_TRAIN: "trainModel",
 };
 
 export const MUTATION_KEYS = {
-  API_SETTINGS: "settingsMutate",
+  API_AUGMENT: "augmentMutate",
+  API_DATATABLE: "dataTableMutate",
   API_EVALUATE: "evaluateMutate",
+  API_SETTINGS: "settingsMutate",
+  API_TRAIN: "trainMutate",
 };
