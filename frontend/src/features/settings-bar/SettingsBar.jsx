26c6695ccac579614d8ce01e91bcc8ca04c6ce4f
CLJiaHao <clarence.lee@murata.com>
2025-05-30 11:43:11 +0800

    FEAT: use placeholder from env and edit style to fit

--- frontend/src/features/settings-bar/SettingsBar.jsx
+++ frontend/src/features/settings-bar/SettingsBar.jsx
@@ -4,7 +4,7 @@ import UtilityPanel from "./subfeatures/utility-panel/UtilityPanel";
 
 const SettingsBar = ({ className }) => {
   return (
-    <div className={cn("flex h-full w-full items-center space-x-4", className)}>
+    <div className={cn("flex h-full w-full items-center", className)}>
       <UtilityPanel />
       <ItemForm />
     </div>
