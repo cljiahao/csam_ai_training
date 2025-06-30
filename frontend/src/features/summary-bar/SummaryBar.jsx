ec1431fef36c1ddbf189817cacbc0abfd3693ca2
Perry Ong <perryongwq@gmail.com>
2025-06-30 10:58:06 +0800

    RFCT: refractor utility panel from sub feature to component

--- frontend/src/features/summary-bar/SummaryBar.jsx
+++ frontend/src/features/summary-bar/SummaryBar.jsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils";
-import UtilityPanel from "./subfeatures/utility-panel/UtilityPanel";
+import UtilityPanel from "./components/UtilityPanel";
 import LabelSwitch from "@/components/widgets/label-switch/LabelSwitch";
-import useUtilityPanel from "./subfeatures/utility-panel/hooks/useUtilityPanel";
+import useLabelSwitch from "./hooks/useLabelSwitch";
 
 const SummaryBar = ({ className, method }) => {
   const {
     state: { isChecked },
     action: { onCheckChange },
-  } = useUtilityPanel({ method });
+  } = useLabelSwitch({ method });
 
   return (
     <div className={cn("flex h-full w-full items-center", className)}>
